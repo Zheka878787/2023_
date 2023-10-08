f17fede676adec5b6bd1d55c68fb909de2621353
Zheka878787 <146108586+Zheka878787@users.noreply.github.com>
2023-10-08 20:21:35 +0300

    draw player

--- game.py
+++ game.py
@@ -1,55 +1,62 @@
 import pygame as pg
 
+
 pg.init()
 
 screen_width, screen_height = 800, 600
 
-
-display = pg.display.set_mode((screen_width, screen_height))
-
-display.fill('blue',(0, 0, screen_width, screen_height))
-
-FPS = 24
+FPS = 24    # frame per second
 clock = pg.time.Clock()
 
-#изображения
+# изображения
 bg_img = pg.image.load('src/background.png')
 icon_img = pg.image.load('src/ufo.png')
 
+display = pg.display.set_mode((screen_width, screen_height))
 pg.display.set_icon(icon_img)
 pg.display.set_caption('Космическое вторжение')
 
-#paint
 sys_font = pg.font.SysFont('arial', 34)
 font = pg.font.Font('src/04B_19.TTF', 48)
 
-display.blit(bg_img, (0, 0))
+# display.fill('blue', (0, 0, screen_width, screen_height))
+display.blit(bg_img, (0, 0))        # image.tr
 
-text_img = sys_font.render('Score 123', True,'white')
-display.blit(text_img,(0,0))
-
-name_text = sys_font.render('Привет, меня зовут Евгений!', True, 'white')
+text_img = sys_font.render('Score 123', True, 'white')
+# display.blit(text_img, (100, 50))
 
 game_over_text = font.render('Game Over', True, 'red')
 w, h = game_over_text.get_size()
-display.blit(game_over_text,(screen_width/2 - w/2, screen_height/2 - h/2))
+# display.blit(game_over_text, (screen_width/2 - w/2, screen_height / 2 - h/2))
 
-#игрок
+# игрок
 player_img = pg.image.load('src/player.png')
 player_width, player_height = player_img.get_size()
 player_gap = 10
-display.blit(game_over_text,(screen_width/2 - w/2, screen_height/2 - h/2))
-
+player_velocity = 1
+player_x = screen_width/2 - player_width/2
+player_y = screen_height  - player_height - player_gap
 
 running = True
 while running:
+    # изменение модели
+    player_x += player_velocity
+
+    # redraw
+    display.blit(bg_img, (0, 0))
+    display.blit(player_img, (player_x, player_y))
     pg.display.update()
 
-
+    # обработка событий
     for event in pg.event.get():
-        #Нажатие на крестик
-        if event.type == pg.QUIT or event.type == pg.KEYDOWN and event.key == pg.K_q:
-            running = False
+        # нажали крестик на окне
+        if event.type == pg.QUIT:
+
+            # нажали на q - quit
+            if event.key == pg.K_q:
+                running = False
+
+            
 
 
     clock.tick(FPS)
